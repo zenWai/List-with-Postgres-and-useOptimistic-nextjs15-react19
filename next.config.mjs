0c58b48a25befbaca9ec73b58c6593a76f276e7e
Presa <ivopresa@gmail.com>
2024-07-17 12:14:38 +0200

    ```security headers```

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,55 @@
+const cspHeader = `
+  default-src 'self';
+  connect-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  img-src 'self' blob: data:;
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  frame-src;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`.replace(/\s{2,}/g, ' ').trim();
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: cspHeader,
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer-when-downgrade",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "deny",
+  },
+  {
+    key: "X-Download-Options",
+    value: "noopen",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
